a96b7105a17c2e06ceec0d603bf6beaf55ca3bd8
David_Gavilanes <davidgavilanesvelez@gmail.com>
2020-07-20 14:57:31 +0200

    search bar up

--- src/components/atoms/Search.jsx
+++ src/components/atoms/Search.jsx
@@ -2,8 +2,7 @@ import React, {useState} from 'react';
 
 export default function Search(){
     const [value, setValue] = useState("")
-
     return(
-        <input onChange={() => setValue(this.event.target.value)} value={value} type='search'/>
+        <input type='text' onChange={(event) => setValue(event.target.value)} value={value} type='search'/>
     )
 }

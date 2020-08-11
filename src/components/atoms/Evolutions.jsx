5e90b08225aa232e4b07b7cba593efbfd92e88af
David_Gavilanes <davidgavilanesvelez@gmail.com>
2020-08-11 11:47:37 +0200

    working on evolutions

--- src/components/atoms/Evolutions.jsx
+++ src/components/atoms/Evolutions.jsx
@@ -4,10 +4,21 @@ import { makeStyles } from "@material-ui/styles";
 const useStyles = makeStyles({
   evolutionWrapper: {
     display: "flex",
-    justifyContent: "space-around",
+    alignItems: "center",
     "& > figure": {
-      width: "20%",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      "&> figcaption": {
+        textTransform: "uppercase",
+        textAlign: "center",
+        marginTop: "1rem",
+      },
     },
+  },
+  arrow: {
+    width: "2rem",
   },
 });
 //WIP
@@ -16,25 +27,42 @@ export default function Evolutions({ data }) {
     var str = url.substr(42);
     return str.replace(/\D/g, "");
   }
+  function howPokemonEvolves(evolutionDetails) {
+    //WIP
+  }
+
+  function printPokemon(pokemon) {
+    return (
+      <div className={classes.evolutionWrapper}>
+        <figure>
+          <img
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${getIdFromUrl(
+              pokemon.species.url
+            )}.png`}
+            alt={pokemon.species.name}
+          />
+          <figcaption>{pokemon.species.name}</figcaption>
+        </figure>
+        {pokemon.evolves_to.length > 0 && (
+          <figure className={classes.evolvesTo}>
+            <img
+              className={classes.arrow}
+              src={process.env.PUBLIC_URL + `/next.png`}
+              alt="evolves to"
+            />
+            <figcaption>
+              {howPokemonEvolves(pokemon.evolution_details[0])}
+            </figcaption>
+          </figure>
+        )}
+        {pokemon.evolves_to.length > 0 && getEvolutions(pokemon.evolves_to)}
+      </div>
+    );
+  }
 
   function getEvolutions(evolutions) {
     return evolutions.map((pokemon) => {
-      return (
-        <div className={classes.evolutionWrapper}>
-          <figure>
-            <img
-              src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${getIdFromUrl(
-                pokemon.species.url
-              )}.png`}
-              alt={pokemon.species.name}
-            />
-            <figcaption>{pokemon.species.name}</figcaption>
-          </figure>
-          {pokemon.evolves_to.length > 0
-            ? getEvolutions(pokemon.evolves_to)
-            : null}
-        </div>
-    
+      return printPokemon(pokemon);
     });
   }
 
@@ -42,18 +70,8 @@ export default function Evolutions({ data }) {
 
   return (
     <div className={classes.evolutionWrapper}>
-      <figure>
-        <img
-          src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${getIdFromUrl(
-            data.chain.species.url
-          )}.png`}
-          alt={data.chain.species.name}
-        />
-        <figcaption>{data.chain.species.name}</figcaption>
-      </figure>
-      {data.chain.evolves_to.length > 0
-        ? getEvolutions(data.chain.evolves_to)
-        : console.log(data.chain.evolves_to)}
+      {printPokemon(data.chain)}
+      {/* {data.chain.evolves_to.length > 0 && getEvolutions(data.chain.evolves_to)} */}
     </div>
   );
 }
